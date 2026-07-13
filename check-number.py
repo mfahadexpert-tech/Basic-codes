9cb2f3323a1cf5719e3618f6ecd67f9c4a832930
mfahadexpert-tech <mfahadexpert@gmail.com>
2026-07-13 18:43:55 +0500

    Add check-number.py script to determine if a number is positive, negative, or zero

---
+++ check-number.py
@@ -0,0 +1,8 @@
+#User se aik number lein aur batayein ke woh Positive hai, Negative hai ya Zero
+num=int(input("Enter a number: "))
+if num>0:
+    print("The number is positive.")
+elif num<0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
